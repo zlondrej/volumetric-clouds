f3c9995be7549f9aba1418f6c170a5f1b1021f61
Ondřej Janošík <j.ondra14@gmail.com>
2015-11-23 17:03:47 +0100

    Enabled backface culling

--- src/Landscape.cpp
+++ src/Landscape.cpp
@@ -133,7 +133,7 @@ void Landscape::render() {
     glUseProgram(renderProgram.getProgram());
 
     glEnable(GL_DEPTH_TEST);
-    //    glEnable(GL_CULL_FACE);
+    glEnable(GL_CULL_FACE);
 
     glClearColor(0.0, 0.7, 1.0, 1.0);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
