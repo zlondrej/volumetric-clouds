cc6b8a68ac5760d85372f60996b8d0bec36a168c
Ondřej Janošík <j.ondra14@gmail.com>
2015-11-23 01:07:29 +0100

    Added opengl debug output

--- src/Main.cpp
+++ src/Main.cpp
@@ -31,6 +31,16 @@ int main(int argc, char **argv) {
     return 0;
 }
 
+void glDebugCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar * message, void * userParam) {
+    bool throwE;
+    cerr << "Debug: " << message << endl;
+
+    throwE = !((type == GL_DEBUG_TYPE_PERFORMANCE) || (severity == GL_DEBUG_SEVERITY_NOTIFICATION));
+
+    if (throwE) {
+        throw GLException(string(message));
+    }
+}
 
 Main::Main() : sdlWindow(NULL), context(NULL), camera(NULL) {
 }
@@ -94,6 +104,11 @@ void Main::init() {
         throw string(SDL_GetError());
     }
 
+
+    glEnable(GL_DEBUG_OUTPUT);
+    glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+
+    glDebugMessageCallback((GLDEBUGPROC) glDebugCallback, NULL);
 
     camera = new Camera(sdlWindow);
 
