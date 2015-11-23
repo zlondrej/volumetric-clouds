595f486f4047bcd0442950e0d4cb7b6511e017ec
Ondřej Janošík <j.ondra14@gmail.com>
2015-11-23 15:50:33 +0100

    Fixed camera movement and orientation

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -3,6 +3,8 @@
 #include <glm/ext.hpp>
 #include <GL/glew.h>
 
+#define PI_HALF_CLAMP (1.57079632679f - 0.0001f)
+
 using namespace pgp;
 using namespace glm;
 
@@ -12,7 +14,7 @@ Camera::Camera(SDL_Window *_window) : position(0., 0, 0), rotation(0, 0) {
 }
 
 vec3 Camera::getViewVector() {
-    vec3 direction = vec3(1, 1, 1);
+    vec3 direction = vec3(0, 0, 1);
 
     direction = rotateX(direction, rotation.x);
     direction = rotateY(direction, rotation.y);
@@ -36,8 +38,8 @@ IEventListener::EventResponse Camera::onEvent(SDL_Event* evt) {
         ivec3 positive, negative;
         SDL_KeyboardEvent *e = &evt->key;
 
-        positive = max(ivec3(0,0,0), movement);
-        negative = min(ivec3(0,0,0), movement);
+        positive = max(ivec3(0, 0, 0), movement);
+        negative = min(ivec3(0, 0, 0), movement);
 
         int active = (evt->type == SDL_KEYDOWN) ? 1 : 0;
 
@@ -49,10 +51,10 @@ IEventListener::EventResponse Camera::onEvent(SDL_Event* evt) {
                 negative.z = active;
                 break;
             case SDLK_a:
-                negative.x = active;
+                positive.x = active;
                 break;
             case SDLK_d:
-                positive.x = active;
+                negative.x = active;
                 break;
             case SDLK_LSHIFT:
                 positive.y = active;
@@ -73,8 +75,11 @@ IEventListener::EventResponse Camera::onEvent(SDL_Event* evt) {
 
         if (SDL_BUTTON_LEFT & e->state) {
             // TODO: Adjust mouse sensitivity coeficient
-            rotation.y += e->xrel * 0.005;
-            rotation.x += e->yrel * 0.005;
+            rotation.y += e->xrel * 0.002;
+            rotation.x -= e->yrel * 0.002;
+
+            // To prevent horizontal turn
+            rotation.x = clamp(rotation.x, -PI_HALF_CLAMP, PI_HALF_CLAMP);
         }
 
         return EVT_PROCESSED;
@@ -83,20 +88,21 @@ IEventListener::EventResponse Camera::onEvent(SDL_Event* evt) {
 }
 
 void Camera::step(float dt) {
+    vec3 direction;
+    if (movement.x == 0 && movement.z == 0) {
+        direction = movement;
+    } else {
+        direction = vec3(movement.x, 0, movement.z);
+        direction = rotateX(direction, rotation.x);
+        direction = rotateY(direction, rotation.y);
 
-    if (movement.x == 0 && movement.y == 0 && movement.z == 0) {
-        return;
+        direction.y += movement.y;
+
+        direction = normalize(direction);
     }
 
-    vec3 direction = vec3(movement.x, 0, movement.z);
-
-    direction = rotateX(direction, rotation.x);
-    direction = rotateY(direction, rotation.y);
-    direction.y += movement.y;
-
-
     // TODO: Add speed coeficient
-    position += normalize(direction) * dt;
+    position += direction * dt * 3.0f;
 }
 
 void Camera::render() {
